9d47d0d874058ace3c352a839998c44e1625efd7
Kat Kim <56666457+katkim0307@users.noreply.github.com>
2020-06-25 10:33:56 -0400

    fixed rendering issue - Name, Username, Website displays.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-//import { render } from '@testing-library/react';
 
 class Lifecycle extends Component {
   constructor() {
@@ -14,22 +13,22 @@ class Lifecycle extends Component {
   // Allows us to attach the data we get from the API call to the state.
   // When we change the state, the component render() will execute again
   // and show the changes made to the DOM.
-  componentDidAmount() {
-    // "fetch" the data from a specific URL, "then" process the data
+  // "fetch" the data from a specific URL, "then" process the data
+  componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-      .then(res => {
+      .then((res) => {
         return res.json();
       })
-      .then(data => {
+      .then((data) => {
         // Check whether data is an array or not
         console.log(Array.isArray(data));
         console.log("Fetched Data: ", data);
         this.setState({
+          isLoaded: true,
           items: data,
-          isLoaded:true,
-        })
-      })
-  }
+        });
+      });
+  };
 
   // 1. Deconstruct the state keys
   // 2. Create a parent div className="App" 
@@ -37,15 +36,27 @@ class Lifecycle extends Component {
   // 4. Render Data (array) as a list {items.map(item => (<li key={item}>{item}</li>))}
   render() {
     let {isLoaded, items} = this.state;
-    return (
-      <div className="App">
-        <div className="Names">
-          <ul>
-          {items.map(item => <li>{item}</li>)}
-          </ul>
+    // Conditional Rendering
+    if(!isLoaded){
+      return <div>Loading...</div>;
+    }
+    
+    else{
+      return (
+        <div className="App">
+          <div className="Names">
+            <ul> {items.map(item => {
+              return (
+                <li key={item.id}>
+                  Name: {item.name} | UserName: {item.username} | 
+                  <a href={`https://${item.website}`}>Website</a>
+                </li>
+              )
+            })}</ul>
+          </div>
         </div>
-      </div>
-    );
+      );
+    };
   };
 };
 
